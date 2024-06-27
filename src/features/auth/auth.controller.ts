9b311bd72f1482f05eac2935d3019032d1f01085
Bao <baonlmz@nec.vn>
2024-06-27 09:06:05 +0700

    fix: update

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -12,7 +12,6 @@ import { LoginDto } from './dto/login.dto';
 import { Public } from 'src/core/decorators/public.decorator';
 import { User } from 'src/core/decorators/user.decorator';
 import { IUser } from 'src/common/interfaces/user.interface';
-import { RoleGuard } from 'src/core/guards/role.guard';
 import { ROLE } from '@prisma/client';
 
 @Controller('auth')
